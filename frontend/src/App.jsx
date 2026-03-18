59fe6df24586f491512516c43678c5da866244f5
gradrrs <artemlal1998@gmail.com>
2026-03-18 23:50:28 +0300

    добавление апи функционала

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Navigation from './components/Navigation'
-import  axios from 'axios'
+import axios from 'axios'
 import CardList from './components/CardList'
- 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [books, setBooks] = useState([])  
 
-  
+useEffect(() => {
+  const fetchBooks = async () => {
+    try {
+      const response = await axios.get('https://openlibrary.org/search.json', {
+        params: { q: 'javascript', limit: 10 }
+      });
+      console.log('response.data:', response.data);
+      console.log('docs:', response.data.docs);
+
+    } catch (error) {
+      console.error('Ошибка:', error);
+    }
+  };
+  fetchBooks();
+}, []);
 
   return (
     <>
-      <Navigation isLogin={true}></Navigation>
-      <CardList></CardList>
+      <Navigation isLogin={true} />
+      <CardList books={books} />
     </>
   )
 }
